3633631089ddeb7266be31d8678aa9d0193aeb51
teevik <teemu.vikoren@gmail.com>
2025-05-11 17:58:08 -0400

    Fix warnings

--- src/main.cpp
+++ src/main.cpp
@@ -5,112 +5,117 @@
 #include "framework/shader_program.h"
 #include <print>
 
-struct Vertex {
-  glm::vec2 position{};
-  glm::vec3 color{1.0f};
-};
-
-// Two vertex attributes: position at 0, color at 1.
-constexpr auto vertex_attributes = std::array{
-  // the format matches the type and layout of data: vec2 => 2x 32-bit floats.
-  vk::VertexInputAttributeDescription2EXT{
-    0, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, position)
-  },
-  // vec3 => 3x 32-bit floats
-  vk::VertexInputAttributeDescription2EXT{
-    1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)
-  },
-};
-
-// One vertex binding at location 0.
-constexpr auto vertex_bindings = std::array{
-  // we are using interleaved data with a stride of sizeof(Vertex).
-  vk::VertexInputBindingDescription2EXT{
-    0, sizeof(Vertex), vk::VertexInputRate::eVertex, 1
-  },
-};
-
-template <typename T> [[nodiscard]] constexpr auto to_byte_array(T const &t) {
-  return std::bit_cast<std::array<std::byte, sizeof(T)>>(t);
-}
-
-auto create_shader(
-  lvk::Renderer &app, fs::path vertex_path, fs::path fragment_path
-) -> lvk::ShaderProgram {
-  auto const vertex_spirv = lvk::read_spir_v(vertex_path);
-  auto const fragment_spirv = lvk::read_spir_v(fragment_path);
-
-  static constexpr auto vertex_input = lvk::ShaderVertexInput{
-    .attributes = vertex_attributes,
-    .bindings = vertex_bindings,
+namespace {
+  struct Vertex {
+    glm::vec2 position{};
+    glm::vec3 color{1.0f};
   };
 
-  auto const shader_info = lvk::ShaderProgram::CreateInfo{
-    .device = *app.device,
-    .vertex_spirv = vertex_spirv,
-    .fragment_spirv = fragment_spirv,
-    .vertex_input = vertex_input,
-    .set_layouts = {},
+  // Two vertex attributes: position at 0, color at 1.
+  constexpr auto vertex_attributes = std::array{
+    // the format matches the type and layout of data: vec2 => 2x 32-bit floats.
+    vk::VertexInputAttributeDescription2EXT{
+      0, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, position)
+    },
+    // vec3 => 3x 32-bit floats
+    vk::VertexInputAttributeDescription2EXT{
+      1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)
+    },
   };
 
-  return lvk::ShaderProgram(shader_info);
-}
-
-auto create_vertex_buffer(lvk::Renderer &app) -> lvk::vma::Buffer {
-  static constexpr auto vertices = std::array{
-    // Vertex{.position = {-0.5f, -0.5f}, .color = {1.0f, 0.0f, 0.0f}},
-    // Vertex{.position = {0.5f, -0.5f}, .color = {0.0f, 1.0f, 0.0f}},
-    // Vertex{.position = {0.0f, 0.5f}, .color = {0.0f, 0.0f, 1.0f}},
-    Vertex{.position = {-0.5f, -0.5f}, .color = {1.0f, 0.0f, 0.0f}},
-    Vertex{.position = {0.5f, -0.5f}, .color = {0.0f, 1.0f, 0.0f}},
-    Vertex{.position = {0.5f, 0.5f}, .color = {0.0f, 0.0f, 1.0f}},
-    Vertex{.position = {-0.5f, 0.5f}, .color = {1.0f, 1.0f, 0.0f}},
+  // One vertex binding at location 0.
+  constexpr auto vertex_bindings = std::array{
+    // we are using interleaved data with a stride of sizeof(Vertex).
+    vk::VertexInputBindingDescription2EXT{
+      0, sizeof(Vertex), vk::VertexInputRate::eVertex, 1
+    },
   };
 
-  static constexpr auto indices = std::array{
-    0u,
-    1u,
-    2u,
-    2u,
-    3u,
-    0u,
-  };
+  template <typename T> [[nodiscard]] constexpr auto to_byte_array(T const &t) {
+    return std::bit_cast<std::array<std::byte, sizeof(T)>>(t);
+  }
 
-  static constexpr auto vertices_bytes = to_byte_array(vertices);
-  static constexpr auto indices_bytes = to_byte_array(indices);
+  auto create_shader(
+    lvk::Renderer &app,
+    const fs::path &vertex_path,
+    const fs::path &fragment_path
+  ) -> lvk::ShaderProgram {
+    auto const vertex_spirv = lvk::read_spir_v(vertex_path);
+    auto const fragment_spirv = lvk::read_spir_v(fragment_path);
 
-  static constexpr auto total_bytes = std::array<std::span<std::byte const>, 2>{
-    vertices_bytes,
-    indices_bytes,
-  };
+    static constexpr auto vertex_input = lvk::ShaderVertexInput{
+      .attributes = vertex_attributes,
+      .bindings = vertex_bindings,
+    };
 
-  auto const buffer_info = lvk::vma::BufferCreateInfo{
-    .allocator = app.allocator.get(),
-    .usage = vk::BufferUsageFlagBits::eVertexBuffer |
-      vk::BufferUsageFlagBits::eIndexBuffer,
-    .queue_family = app.gpu.queue_family,
-  };
+    auto const shader_info = lvk::ShaderProgram::CreateInfo{
+      .device = *app.device,
+      .vertex_spirv = vertex_spirv,
+      .fragment_spirv = fragment_spirv,
+      .vertex_input = vertex_input,
+      .set_layouts = {},
+    };
 
-  // vbo = vma::create_device_buffer(
-  //   allocator.get(),
-  //   vk::BufferUsageFlagBits::eVertexBuffer |
-  //     vk::BufferUsageFlagBits::eIndexBuffer,
-  //   create_command_block(),
-  //   total_bytes
-  // );
+    return lvk::ShaderProgram(shader_info);
+  }
 
-  auto command_block =
-    lvk::CommandBlock{*app.device, app.queue, *app.cmd_block_pool};
+  auto create_vertex_buffer(lvk::Renderer &app) -> lvk::vma::Buffer {
+    static constexpr auto vertices = std::array{
+      // Vertex{.position = {-0.5f, -0.5f}, .color = {1.0f, 0.0f, 0.0f}},
+      // Vertex{.position = {0.5f, -0.5f}, .color = {0.0f, 1.0f, 0.0f}},
+      // Vertex{.position = {0.0f, 0.5f}, .color = {0.0f, 0.0f, 1.0f}},
+      Vertex{.position = {-0.5f, -0.5f}, .color = {1.0f, 0.0f, 0.0f}},
+      Vertex{.position = {0.5f, -0.5f}, .color = {0.0f, 1.0f, 0.0f}},
+      Vertex{.position = {0.5f, 0.5f}, .color = {0.0f, 0.0f, 1.0f}},
+      Vertex{.position = {-0.5f, 0.5f}, .color = {1.0f, 1.0f, 0.0f}},
+    };
 
-  auto vbo = lvk::vma::create_device_buffer(
-    buffer_info, std::move(command_block), total_bytes
-  );
+    static constexpr auto indices = std::array{
+      0u,
+      1u,
+      2u,
+      2u,
+      3u,
+      0u,
+    };
 
-  return vbo;
+    static constexpr auto vertices_bytes = to_byte_array(vertices);
+    static constexpr auto indices_bytes = to_byte_array(indices);
 
-  // // Host buffers have a memory-mapped pointer available to memcpy data to.
-  // std::memcpy(vbo.get().mapped, vertices.data(), sizeof(vertices));
-}
+    static constexpr auto total_bytes =
+      std::array<std::span<std::byte const>, 2>{
+        vertices_bytes,
+        indices_bytes,
+      };
+
+    auto const buffer_info = lvk::vma::BufferCreateInfo{
+      .allocator = app.allocator.get(),
+      .usage = vk::BufferUsageFlagBits::eVertexBuffer |
+        vk::BufferUsageFlagBits::eIndexBuffer,
+      .queue_family = app.gpu.queue_family,
+    };
+
+    // vbo = vma::create_device_buffer(
+    //   allocator.get(),
+    //   vk::BufferUsageFlagBits::eVertexBuffer |
+    //     vk::BufferUsageFlagBits::eIndexBuffer,
+    //   create_command_block(),
+    //   total_bytes
+    // );
+
+    auto command_block =
+      lvk::CommandBlock{*app.device, app.queue, *app.cmd_block_pool};
+
+    auto vbo = lvk::vma::create_device_buffer(
+      buffer_info, std::move(command_block), total_bytes
+    );
+
+    return vbo;
+
+    // // Host buffers have a memory-mapped pointer available to memcpy data to.
+    // std::memcpy(vbo.get().mapped, vertices.data(), sizeof(vertices));
+  }
+} // namespace
 
 auto main() -> int {
   // TODO(teevik) Configurable
